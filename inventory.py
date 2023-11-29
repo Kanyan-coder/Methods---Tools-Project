a1a8b75d04c831dc7c9e27407c0b3a101eded67a
philstew <149547855+philstew@users.noreply.github.com>
2023-11-28 19:02:01 -0600

    changed inventory
    - updated the get book method so that it requests an ISBN rather than a title, it returns, the ISBN, Title, Quantity bought and Total price. 
    - updated the add book function to add the price to a new book. 
    - updated the showInventory function so that it prints out the ISBN too.

--- inventory.py
+++ inventory.py
@@ -14,33 +14,29 @@ class Inventory:
 
 
     def showInventory(self):
-        self.cursor.execute("SELECT Name, Quantity, Price FROM {}".format(self.tableName))
+        self.cursor.execute("SELECT ISBN, Name, Quantity, Price FROM {}".format(self.tableName))
         result = self.cursor.fetchall()
 
         if result:
-            print("{:<30} {:<10} {:<10}".format("Title", "Quantity", "Price"))
-            print("=" * 50)
+            print("{:<15} {:<30} {:<10} {:<10}".format("ISBN", "Title", "Quantity", "Price"))
+            print("=" * 65)
             for item in result:
-                title, quantity, price = item
-                print("{:<30} {:<10} ${:<10}".format(title, quantity, price))
+                isbn, title, quantity, price = item
+                print("{:<15} {:<30} {:<10} ${:<10}".format(isbn, title, quantity, price))
         else:
             print("The inventory is empty.")
 
     def searchInventory(self):
         search_term = input("Enter a search term: ")
-        self.cursor.execute("SELECT * FROM {} WHERE Name LIKE ?".format(self.tableName), ('%' + search_term + '%',))
+        self.cursor.execute("SELECT ISBN, Name, Quantity, Price FROM {} WHERE Name LIKE ?".format(self.tableName), ('%' + search_term + '%',))
         result = self.cursor.fetchall()
 
         if result:
-            print("Search results:")
+            print("{:<15} {:<30} {:<10} {:<10}".format("ISBN", "Title", "Quantity", "Price"))
+            print("=" * 65)
             for item in result:
-                title = item[1]
-                quantity = item[2]
-
-                if quantity > 0:
-                    print(f"{title} - Quantity: {quantity}")
-                else:
-                    print(f"{title} - Out of stock")
+                isbn, title, quantity, price = item
+                print("{:<15} {:<30} {:<10} ${:<10}".format(isbn, title, quantity, price))
         else:
             print("No matching items found in the inventory.")
 
@@ -48,9 +44,10 @@ class Inventory:
         isbn = int(input("Enter the ISBN of the book: "))
         title = input("Enter the title of the book: ")
         quantity = int(input("Enter the quantity of the book: "))
+        price = float(input("Enter the price of the book: "))
 
-        data = (isbn, title, quantity)
-        self.cursor.execute("INSERT INTO {} (ISBN, Name, Quantity) VALUES (?, ?, ?)".format(self.tableName), data)
+        data = (isbn, title, quantity, price)
+        self.cursor.execute("INSERT INTO {} (ISBN, Name, Quantity, Price) VALUES (?, ?, ?, ?)".format(self.tableName), data)
         self.connection.commit()
         print("Book added to inventory.")
 
@@ -87,52 +84,31 @@ class Inventory:
         else:
             return None
 
-    def getBook(self, title):
-        current_quantity = self.getQuantity(title)
+    def getBook(self):
+        isbn = int(input("Enter the ISBN of the book to get: "))
+        quantity_requested = int(input("Enter the quantity you want to get: "))
 
-        if current_quantity > 0:
-            self.remQuantity(title, 1)
-            price = self.getPrice(title)
+        self.cursor.execute("SELECT ISBN, Name, Quantity, Price FROM {} WHERE ISBN = ?".format(self.tableName), (isbn,))
+        result = self.cursor.fetchone()
 
-            if price is not None:
-                print("Book '{}' obtained. Price: ${}".format(title, price))
-                self.cursor.execute("SELECT * FROM {} WHERE Name = ?".format(self.tableName), (title,))
-                book = self.cursor.fetchone()
-                return book, price
+        if result:
+            isbn, title, current_quantity, price = result
+
+            if current_quantity >= quantity_requested:
+                total_price = price * quantity_requested
+
+                self.remQuantity(title, quantity_requested)
+
+                print("\nBook Details:")
+                print("ISBN: {}".format(isbn))
+                print("Title: {}".format(title))
+                print("Quantity: {}".format(quantity_requested))
+                print("Price per unit: ${}".format(price))
+                print("Total Price: ${}".format(total_price))
+                return isbn, title, quantity_requested, total_price
             else:
-                print("Error: Price not found for book '{}'.".format(title))
+                print("Error: Not enough quantity in stock.")
                 return None
         else:
-            print("Error: Book '{}' is out of stock.".format(title))
+            print("Error: Book with ISBN {} not found.".format(isbn))
             return None
-def main():
-    inventory_sys = Inventory("Project.db", "Inventory")
-
-    while True:
-        print("\n===== Inventory Management Menu =====")
-        print("1. Show Inventory")
-        print("2. Add Book")
-        print("3. Remove Book")
-        print("4. Add Quantity")
-        print("5. Search Inventory")
-        print("6. Get Book Details with Price")
-        print("0. Exit")
-
-        choice = input("Enter your choice (0-6): ")
-        if choice == "0":
-            break
-        elif choice == "1":
-            inventory_sys.showInventory()
-        elif choice == "2":
-            inventory_sys.addBook()
-        elif choice == "3":
-            inventory_sys.remBook()
-        elif choice == "4":
-            inventory_sys.addQuantity()
-        elif choice == "5":
-            inventory_sys.searchInventory()
-        elif choice == "6":
-            book_title = input("Enter the title of the book to get details: ")
-            book_info = inventory_sys.getBook(book_title)
-if __name__ == "__main__":
-    main()
